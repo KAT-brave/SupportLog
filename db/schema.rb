39e067fc34236d36c9618ad01d3f8138b245a4a8
KAI <brave.you.and.me@gmail.com>
2026-03-28 11:19:58 +0900

    users テーブルに approved_at を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_28_015845) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_28_021718) do
   create_table "sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "ip_address"
@@ -21,6 +21,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_28_015845) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin"
+    t.datetime "approved_at"
     t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
