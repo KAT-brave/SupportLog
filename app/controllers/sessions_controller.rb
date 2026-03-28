a855df50b8dcb832b993b72877c27236ba438c25
KAI <brave.you.and.me@gmail.com>
2026-03-28 11:55:09 +0900

    未承認ユーザーはログインできないようにする

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate_by(params.permit(:email_address, :password))
+    user = User.find_by(email_address: params[:email_address])
+
+    if user&.authenticate(params[:password])
+      unless user.approved?
+        redirect_to new_session_path, alert: "現在、管理者承認待ちです。"
+        return
+      end
+
       start_new_session_for user
-      redirect_to after_authentication_url
+      redirect_to root_path, notice: "ログインしました"
     else
-      redirect_to new_session_path, alert: "Try another email address or password."
+      redirect_to new_session_path, alert: "メールアドレスまたはパスワードが正しくありません"
     end
   end
 
