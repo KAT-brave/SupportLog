78f4d8c0a24b9c94b90a132f670a6c5dfe62ced0
KAI <brave.you.and.me@gmail.com>
2026-04-21 07:49:50 +0900

    認証簡易テストメール

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "inquiries/edit"
   get "inquiries/update"
   root "home#index"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 
   resource :registration, only: %i[new create]
   resource :session
