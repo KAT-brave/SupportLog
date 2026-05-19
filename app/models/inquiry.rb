b5920f03fdb704235f1a3e3a6419e433a64d8ed1
KAI <brave.you.and.me@gmail.com>
2026-05-19 23:56:50 +0900

    Fix inquiry model end position

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -30,23 +30,22 @@ class Inquiry < ApplicationRecord
     update!(deleted_at: Time.current, delete_reason: reason)
   end
 
-    def status_i18n
-      {
-        "unhandled" => "未対応",
-        "in_progress" => "対応中",
-        "completed" => "完了"
-      }[status]
-    end
+  def status_i18n
+    {
+      "unhandled" => "未対応",
+      "in_progress" => "対応中",
+      "completed" => "完了"
+    }[status]
+  end
 
-    def priority_i18n
-      {
-       "incident" => "障害",
-        "high" => "高",
-        "medium" => "中",
-        "low" => "低"
-      }[priority]
-    end
-end
+  def priority_i18n
+    {
+      "incident" => "障害",
+      "high" => "高",
+      "medium" => "中",
+      "low" => "低"
+    }[priority]
+  end
 
   private
 
@@ -60,3 +59,4 @@ end
   def features_must_be_present
     errors.add(:features, "を1つ以上選択して下さい") if features.blank?
   end
+end
